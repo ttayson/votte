71a650b20b8761a8cb038c6b1ee8218ad4685cdc
Talles Tayson <tallestayson@gmail.com>
2020-11-14 15:52:17 -0300

    correções

--- models/Eleicao.js
+++ models/Eleicao.js
@@ -2,15 +2,29 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 
-const Eleicao = new.Schema({
+const Eleicao = new Schema({
     nome: {
         type: String,
         require:true
     },
-    descricao: {
+    cargo: {
         type: String,
         require: true
-    }
+    },
+    descricao: {
+        type: String,
+        require: false
+    },
+    status: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    candidatos: [{
+        type: Schema.Types.ObjectId,
+        ref: "candidato",
+        require: true
+    }]
     // date: {
     //     type: Date,
     //     default: Date.now()
--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,13 @@ const express = require('express')
 const mongoose = require("mongoose")
 
 require("../models/Candidato")
+require("../models/Eleicao")
 
 const Candidato =mongoose.model("candidato")
+
+const Eleicao =mongoose.model("eleicao")
+
+
 
 const router = express.Router()
 
@@ -28,9 +33,123 @@ router.get('/finalizadas', (req, res) => {
     res.render("admin/finalizadas")
 })
 
-// router.get('/eleicao', (req, res) => {
-//     res.render("admin/eleicao")
-// })
+router.get('/eleicao', (req, res) => {
+    Eleicao.find().populate("candidatos").then((eleicao) =>{
+        res.render("admin/eleicao", {eleicao: eleicao})
+    })
+    
+})
+
+router.get('/eleicao/edit/:id', (req, res) => {
+    Eleicao.findOne({_id:req.params.id}).populate("candidatos").then((eleicao) =>{
+        Candidato.find().then((candidato) => {
+        res.render("admin/editeleicao", {eleicao: eleicao, candidato: candidato})
+    })
+
+    }).catch((err)=>{
+        console.log("Erro ao procurar eleição")
+    })
+    
+            
+})
+
+router.post('/eleicao/edit/del/', (req, res) => {
+    Eleicao.remove({_id:req.body.id}).then(() =>{
+        res.redirect("/admin/eleicao")
+        console.log("Eleição removida")
+    }).catch((err)=>{
+        console.log("Erro ao procurar eleição")
+    })
+    
+            
+})
+
+
+router.post('/eleicao/edit/', (req, res) => {
+    Eleicao.findOne({_id:req.body.id}).then((eleicao) =>{
+        Candidato.find().then((candidato) => {
+
+        var erros = []
+
+        if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+            erros.push({text: "Nome para eleição inválido"})
+        }
+
+        if(!req.body.cargo || typeof req.body.cargo == undefined || req.body.cargo == null){
+            erros.push({text: "Nome para o cargo inválido"})
+        }
+
+        if(!req.body.candidatos || typeof req.body.candidatos == undefined || req.body.candidatos == null){
+            erros.push({text: "Selecione pelo menos um candidato"})
+        }
+        
+        if(erros.length > 0){
+            res.render("admin/editeleicao", {erros: erros, eleicao: eleicao, candidato: candidato})
+
+        }else{
+
+            console.log(eleicao)
+
+
+                eleicao.nome = req.body.nome
+                eleicao.cargo = req.body.cargo
+                eleicao.descricao = req.body.descricao
+                eleicao.candidatos = req.body.candidatos
+
+        
+                eleicao.save().then(() => {
+                    console.log("Eleição editada com Sucesso")
+                    res.redirect("/admin/eleicao")
+            }).catch((err) => {
+                    console.log("Erro ao Salvar no Banco (Eleição)")
+            });
+
+        }
+    
+    })
+
+    })
+            
+})
+
+router.get('/novaeleicao', (req, res) => {
+    Candidato.find().then((candidatos) => {
+        res.render("admin/novaeleicao", {candidatos: candidatos})
+    })
+})
+
+router.post('/novaeleicao/add', (req, res) => {
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({text: "Nome para eleição inválido"})
+    }
+
+    if(!req.body.cargo || typeof req.body.cargo == undefined || req.body.cargo == null){
+        erros.push({text: "Nome para o cargo inválido"})
+    }
+    
+    if(erros.length > 0){
+        res.render("admin/novaeleicao", {erros: erros})
+    }else{
+
+        const novaEleicao = {
+            nome: req.body.nome,
+            cargo: req.body.cargo,
+            descricao: req.body.descricao,
+            candidatos: req.body.canditados
+        }
+    
+        new Eleicao(novaEleicao).save().then(() => {
+            console.log("Eleição cadastrada com Sucesso")
+            res.redirect("/admin/eleicao")
+        }).catch((err) => {
+            console.log("Erro ao Salvar no Banco (Candidato)")
+        });
+
+    }
+    
+})
 
 router.get('/candidato', (req, res) => {
     Candidato.find().then((candidato) =>{
