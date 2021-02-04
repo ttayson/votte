c882f32a50337d1a1af59a875ce56ae3d6781c97
Talles Tayson Saldanha de Farias <talles@caern.local>
2021-02-04 16:12:00 -0300

    db

--- app.js
+++ app.js
@@ -75,8 +75,8 @@ const hbs = handlebars.create({
 
     //mongoose
     mongoose.Promise = global.Promise;
-    // mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@ttayson.cf/'+process.env.DB_NAME, {
-    mongoose.connect('mongodb://localhost/votta2', {
+    mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@ttayson.cf/'+process.env.DB_NAME, {
+    // mongoose.connect('mongodb://localhost/votta2', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
